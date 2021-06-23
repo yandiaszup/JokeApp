5f95870fd7f6e95d5fb362a354fe75e14649b3bf
Yan <yan.dias@zup.com.br>
2021-06-23 17:08:47 -0300

    first commit

---
+++ JokeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JokeApp
+//
+//  Created by Yan Dias on 23/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JokeApp/JokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeAppApp.swift
+//  JokeApp
+//
+//  Created by Yan Dias on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct JokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
